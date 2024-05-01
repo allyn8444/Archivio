4cd6ed4962229f5568f16fa3e0288ba4edb9ba23
allyn ralf ledesma <dgtlmonsieur@gmail.com>
2024-05-02 01:29:47 +0800

    refactor: footer layout and content

--- app/pages/includes/footer.php
+++ app/pages/includes/footer.php
@@ -1,21 +1,39 @@
-  </main>
+<!-- TODO: add ABOUT US section before footer -->
+</main>
 
-  <!-- TODO: change text colors (current: text-primary) -->
-  <div class="container">
-    <footer class="py-5">
-      <div class="row">
-        <div class="col-6 col-md-2 mb-3">
-          <h5>Useful Links</h5>
+<!-- TODO: change text colors (current: text-primary) -->
+<div class="" style="background-color: red;">
+  <footer class="py-2">
+    <div class="d-flex flex-column flex-lg-row align-items-center flex-wrap text-center">
+      <div class="flex-fill"><img src="<?= ROOT ?>/assets/images/logo.svg" alt=""></div>
+
+      <!--  -->
+      <div class="flex-fill text-center text-lg-start" style="width: 100%; max-width: 500px;">
+        <p>This website was created as a subject requirement under CCS 222: Web Design and Development.</p>
+        <!-- <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">About</a></li>
+        </ul> -->
+      </div>
+      <!--  -->
+      <div class="d-sm-flex">
+        <div class="flex-fill">
+          <h5>Developers</h5>
           <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Home</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Pricing</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">FAQs</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">About</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Ledesma, Allyn Ralf</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Brey, Justin Jones</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Cabales, Lean Vince</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Calix, Zuriel Eliazar</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Juanico, Michelle Joy</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Rendon, Jethro</a></li>
           </ul>
         </div>
 
-        <div class="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
+        <div class="flex-fill">
+          <h5>Links</h5>
           <ul class="nav flex-column">
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Home</a></li>
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Features</a></li>
@@ -24,40 +42,30 @@
             <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">About</a></li>
           </ul>
         </div>
-
-        <div class="col-6 col-md-2 mb-3">
-          <h5>Section</h5>
-          <ul class="nav flex-column">
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Home</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Features</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Pricing</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">FAQs</a></li>
-            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">About</a></li>
-          </ul>
-        </div>
-
-        <div class="col-md-5 offset-md-1 mb-3">
-          <form>
-            <h5>Subscribe to our newsletter</h5>
-            <p>Monthly digest of what's new and exciting from us.</p>
-            <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-              <label for="newsletter1" class="visually-hidden">Email address</label>
-              <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-              <button class="btn btn-primary" type="button">Subscribe</button>
-            </div>
-          </form>
-        </div>
       </div>
+      <!--  -->
+    </div>
 
-      <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p>&copy; 2022 Company, Inc. All rights reserved.</p>
+    <div class="d-flex flex-column flex-sm-row justify-content-center py-4 my-4 border-top">
+      <p>&copy; 2022 Company, Inc. All rights reserved.</p>
 
-      </div>
-    </footer>
-  </div>
+    </div>
+  </footer>
+</div>
 
-  <script src="<?= ROOT ?>/assets/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="<?= ROOT ?>/assets/bootstrap/js/bootstrap.bundle.js"></script>
 
-  </body>
+</body>
 
-  </html>
+<!-- <div class="g-col-6 g-col-md-2 mb-3">
+        <h5>Section</h5>
+        <ul class="nav flex-column">
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Home</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Features</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">Pricing</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">FAQs</a></li>
+          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-primary">About</a></li>
+        </ul>
+      </div> -->
+
+</html>
